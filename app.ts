b834bb44e0c0290fde9aac16a1ab5ae51f8e86ca
RehanKhaz <165584969+RehanKhaz@users.noreply.github.com>
2024-04-18 00:42:08 +0500

    Simple Project to count the words.

---
+++ app.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node 
+import chalk from "chalk";
+import inquirer from "inquirer";
+console.log(
+  " \n Welcome to the " +
+    chalk.yellow.bold.italic("Words Counter") +
+    " Project using" +
+    chalk.green.bold.italic(" Inquirer, chalk and Node.js:")
+);
+const askUser: {
+  Sentence: string;
+} = await inquirer.prompt([
+  {
+    name: "Sentence",
+    type: "input",
+    message: "Write the sentence in order to count the number of words.",
+  },
+]);
+
+let Usersentence = askUser.Sentence.trim().split(" ");
+console.log(
+  chalk.bold.italic.redBright(
+    ` \n \t => The number of words in the given sentence is ${Usersentence.length}.`
+  )
+);
